de0ce293c48beefd2341b828401b7bf1a3ea7084
Huy Hoang <huyhoang.ng203@gmail.com>
2024-09-24 23:22:19 +0700

    Da hoan thanh cau truc noi dung + optimize , Minh chung

--- App.js
+++ App.js
@@ -18,14 +18,19 @@ export default function App() {
             <Text style={{fontSize:20}}>(Xem 828 đánh giá)</Text>
         </View>
         <View style={styles.prices}>
-            <Text style={{marginRight: 15,fontWeight:'bold',fontSize:35,color:'red'}} >1.790.000</Text>
+            <Text style={{marginRight: 35,fontWeight:'bold',fontSize:35}} >1.790.000</Text>
             <Text style={{marginTop: 8,fontWeight:'bold',fontSize:27,color:'grey',textDecorationLine:'line-through'}} >1.790.000</Text>
         </View>
 
-        <TouchableOpacity style={{flexDirection:'row', alignItems:'center',justifyContent:'center',borderColor: 'black',borderWidth:1,backgroundColor:'white',borderRadius:10,height:50,width:'90%'}}>
+        <View style={{flexDirection:'row',justifyContent:'flex-start',alignItems:'center'}} >
+            <Text style={{marginRight: 150,fontWeight:'bold',fontSize:20,color:'red'}} >Ở đâu rẻ hơn hoàn tiền</Text>
+            <Image source={require('./assets/imgs/help_icon.png')} style={{position:'absolute',left:220}} />
+        </View>
+
+        <TouchableOpacity style={{flexDirection:'row', alignItems:'center',marginTop:10,justifyContent:'center',borderColor: 'black',borderWidth:1,backgroundColor:'white',borderRadius:10,height:50,width:'90%'}}>
           <View style={{flexDirection:'row',alignItems:'center'}}>
             <Text style={{alignSelf: 'center',fontSize:24}} >4 Màu - Chọn màu</Text>
-            <Image source={require('./assets/imgs/Vector.png')} style={{position:'absolute',left:240}} />
+            <Image source={require('./assets/imgs/Vector.png')} style={{position:'absolute',left:240,}} />
           </View>
         </TouchableOpacity>
 
